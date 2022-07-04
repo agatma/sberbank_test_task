c5ef6c61596854a8c1f1174279dd8a964e831d11
Agatanov Madikhan <i@madikhan.ru>
2022-07-04 12:53:48 +0500

    Update create_view.py

--- service/utils/create_view.py
+++ service/utils/create_view.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Table, MetaData
 from sqlalchemy.sql import text
 from sqlalchemy_views import CreateView
-from service.utils.queries import SELECT_DATA_FOR_VIEW
+from queries import SELECT_DATA_FOR_VIEW
 
 
 view = Table("delta_view", MetaData())
